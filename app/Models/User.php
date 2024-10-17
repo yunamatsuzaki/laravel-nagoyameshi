2d754d423aa8561f56b2008363c5ca755671fe7f
yunamatsuzaki <yunamatsuzaki@icloud.com>
2024-10-17 22:07:18 +0900

    会員側の認証を実装

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,9 +19,15 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'kana',
         'email',
         'password',
-    ];
+        'postal_code',
+        'address',
+        'phone_number',
+        'birthday',
+        'occupation',
+];
 
     /**
      * The attributes that should be hidden for serialization.
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,15 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+             'name' => fake()->name(),
+             'kana' => fake()->kanaName(),
+             'email' => fake()->unique()->safeEmail(),
+             'email_verified_at' => now(),
+             'password' => static::$password ??= Hash::make('password'),
+             'remember_token' => Str::random(10),
+             'postal_code' => fake()->postcode(),
+             'address' => fake()->address(),
+             'phone_number' => fake()->phoneNumber(),
         ];
     }
 
